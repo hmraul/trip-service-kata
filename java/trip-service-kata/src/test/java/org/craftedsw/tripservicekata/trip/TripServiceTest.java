dae9960bc60b33dbf574a10d48400e6e7e1ea461
Raul Herranz Muñoz <hmraul@gmail.com>
2015-07-11 19:38:12 +0200

    Testing user that the're not friends

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -4,9 +4,17 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class TripServiceTest {
 
-    private static final User A_USER = null;
+    private static final User A_NOT_LOGGED_USER = null;
+    private User A_LOGGED_USER = null;
+    private User A_USER = null;
+
 
     @Test (expected = UserNotLoggedInException.class)
     public void should_throw_UserNotLoggedInException_when_not_logged() {
@@ -16,12 +24,23 @@ public class TripServiceTest {
 
     }
 
+    @Test
+    public void should_return_an_empty_list_if_users_are_not_friends() {
+        A_LOGGED_USER = new User();
+        A_USER = new User();
+
+        TripServiceTesteable tripService = new TripServiceTesteable();
+        List<Trip> trips = tripService.getTripsByUser(A_USER);
+
+        assertThat(trips.size(), is(0));
+    }
+
 
     public class TripServiceTesteable extends TripService {
 
         @Override
         protected User getLoggedUser() {
-            return null;
+            return A_LOGGED_USER;
         }
     }
 	

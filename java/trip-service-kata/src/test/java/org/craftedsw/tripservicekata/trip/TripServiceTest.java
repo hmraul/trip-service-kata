cddba466df8e33cda8e15cb22dedb70b7d53d715
Raul Herranz Muñoz <hmraul@gmail.com>
2015-07-11 19:41:26 +0200

    First refactor on test

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -2,6 +2,7 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -14,22 +15,25 @@ public class TripServiceTest {
     private static final User A_NOT_LOGGED_USER = null;
     private User A_LOGGED_USER = null;
     private User A_USER = null;
+    private TripServiceTesteable tripService;
+
+
+    @Before
+    public void setUp() {
+        A_USER = new User();
+        tripService = new TripServiceTesteable();
+    }
 
 
     @Test (expected = UserNotLoggedInException.class)
     public void should_throw_UserNotLoggedInException_when_not_logged() {
-
-        TripServiceTesteable tripService = new TripServiceTesteable();
         tripService.getTripsByUser(A_USER);
-
     }
 
     @Test
     public void should_return_an_empty_list_if_users_are_not_friends() {
         A_LOGGED_USER = new User();
-        A_USER = new User();
 
-        TripServiceTesteable tripService = new TripServiceTesteable();
         List<Trip> trips = tripService.getTripsByUser(A_USER);
 
         assertThat(trips.size(), is(0));
